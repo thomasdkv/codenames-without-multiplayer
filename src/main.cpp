5213353517ff045fafaf1c3cfe5caaa1d51de253
AshtonF04 <ashtonfranklin772@gmail.com>
2025-03-11 17:17:10 -0400

    MBA-38 styled main menu buttons, fixed layout and positioning

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
 
   MainWindow game; // Create an instance of the Minesweeper game
   game.setWindowTitle("Codenames"); // Set the window title
+  game.resize(1000, 600);
   game.show();                      // Display the game window
 
   return app.exec(); // Enter the Qt event loop
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,35 +1,54 @@
 #include "mainwindow.h"
 #include "pregame.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
-  // Create central widget and layout
-  centralWidget = new QWidget(this);
-  layout = new QVBoxLayout(centralWidget);
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent) {
+    // Create central widget and layout
+    centralWidget = new QWidget(this);
+    layout = new QVBoxLayout(centralWidget);
 
-  // Create a button to open the PreGame window
-  pushButton = new QPushButton("Open PreGame Window", centralWidget);
-  layout->addWidget(pushButton);
+    // Button to open local play pre game lobby
+    localPlayButton = new QPushButton("Local Play", centralWidget);
+    localPlayButton->setFixedSize(200, 50);
+    localPlayButton->move(400, 200); // Set y-position to 50
+    
+    // Button to open online play pre game lobby
+    onlinePlayButton = new QPushButton("Online Play", centralWidget);
+    onlinePlayButton->setFixedSize(200, 50);
+    onlinePlayButton->move(400, 275); // Set y-position to 50
 
-  // Create the PreGame window
-  preGameWindow = new PreGame();
+    // Button to open statistics menu
+    statsButton = new QPushButton("Statistics", centralWidget);
+    statsButton->setFixedSize(200, 50);
+    statsButton->move(400, 350); // Set y-position to 50
 
-  // Connect button to open PreGame window
-  connect(pushButton, &QPushButton::clicked, this, &MainWindow::openPreGame);
+    localPlayButton->setStyleSheet("background-color: blue;");
+    onlinePlayButton->setStyleSheet("background-color: blue;");
+    statsButton->setStyleSheet("background-color: blue;");
 
-  // Connect signal from PreGame to show MainWindow when back button is clicked
-  connect(preGameWindow, &PreGame::backToMainWindow, this,
-          &MainWindow::showMainWindow);
-
-  // Set central widget and layout
-  centralWidget->setLayout(layout);
-  setCentralWidget(centralWidget);
+    // Create the PreGame window
+    preGameWindow = new PreGame();
+    
+    // Connect button to open PreGame window
+    connect(localPlayButton, &QPushButton::clicked, this, &MainWindow::openPreGame);
+    
+    // Connect signal from PreGame to show MainWindow when back button is clicked
+    connect(preGameWindow, &PreGame::backToMainWindow, this, &MainWindow::showMainWindow);
+    
+    // Set central widget and layout
+    centralWidget->setLayout(layout);
+    setCentralWidget(centralWidget);
 }
 
-MainWindow::~MainWindow() { delete preGameWindow; }
+MainWindow::~MainWindow() {
+    delete preGameWindow;
+}
 
 void MainWindow::openPreGame() {
-  this->hide();
-  preGameWindow->show();
+    this->hide();
+    preGameWindow->show();
 }
 
-void MainWindow::showMainWindow() { this->show(); }
+void MainWindow::showMainWindow() {
+    this->show();
+}
