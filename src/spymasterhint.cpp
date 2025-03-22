881a700230c165313a047e05c0fb093651243eec
Matthew <matthewmarbina87@gmail.com>
2025-03-22 10:03:37 -0400

    MBA-13 initialized textValidator and linked it to the QLineEdit, implemented textToUppercase function, removed need to check if the hint is empty since the user cannot click the button until text is typed, cleaned up + added new styling

--- src/spymasterhint.cpp
+++ src/spymasterhint.cpp
@@ -7,16 +7,26 @@ SpymasterHint::SpymasterHint(QWidget* parent) : QWidget(parent) {
     // Create the LineEdit, SpinBox, and Button widgets
     hintLineEdit = new QLineEdit(this);
     hintLineEdit->setPlaceholderText("Type your clue here");
+    hintLineEdit->setFixedSize(300, 35);
+    hintLineEdit->setStyleSheet("text-transform: uppercase; padding: 5px; font-size: 16px;");
 
-    QSpinBox* numberSpinBox = new QSpinBox(this);
+    textValidator = new QRegularExpressionValidator(QRegularExpression("[A-Za-z]+"), this);
+    hintLineEdit->setValidator(textValidator);
+
+    numberSpinBox = new QSpinBox(this);
     numberSpinBox->setRange(0, 9);
     numberSpinBox->setSpecialValueText("∞"); // Special value for unlimited guesses
+    numberSpinBox->setFixedSize(50, 35);
 
-    QPushButton* giveClueButton = new QPushButton("Give Clue", this);
-    giveClueButton->setFixedSize(200, 100);
-    giveClueButton->setStyleSheet("font-size: 16px;");
-    giveClueButton->setStyleSheet("background-color:rgb(54, 182, 58); color: black;");
+    giveClueButton = new QPushButton("Give Clue", this);
+    giveClueButton->setFixedSize(100, 35);
+    giveClueButton->setStyleSheet(
+        "font-weight: bold; font-size: 16px; color: white; border-radius: 5px;"
+        "background-color:rgb(54, 182, 58);"
+        "QPushButton:hover { background-color: rgb(119, 236, 117); }");
     giveClueButton->setEnabled(false);
+    giveClueButton->setCursor(Qt::PointingHandCursor);
+
 
     // Add the widgets to the layout
     layout->addWidget(hintLineEdit);
@@ -26,24 +36,27 @@ SpymasterHint::SpymasterHint(QWidget* parent) : QWidget(parent) {
 
     // Connect the button to the submitHint slot
     connect(giveClueButton, &QPushButton::clicked, this, &SpymasterHint::submitHint);
+    connect(hintLineEdit, &QLineEdit::textChanged, this, &SpymasterHint::updateButtonClickable);
 }
 
 SpymasterHint::~SpymasterHint() {
-    // Let Qt handle widget deletion
+    // Deconstructor for the SpymasterHint class, does nothing
 }
 
 void SpymasterHint::submitHint() {
     QString hint = hintLineEdit->text();
     int number = numberSpinBox->value();
-    if (hint.isEmpty()) {
-        QMessageBox::warning(this, "Missing Hint", "Please enter a hint");
-        return;
-    } else {
-        emit hintSubmitted(hint, number);
-    }
+    emit hintSubmitted(hint, number);
+    hintLineEdit->clear();
+    numberSpinBox->setSpecialValueText("∞");
 }
 
 void SpymasterHint::updateButtonClickable() {
     giveClueButton->setEnabled(!hintLineEdit->text().isEmpty());
-    giveClueButton->setCursor(Qt::PointingHandCursor);
+}
+
+void SpymasterHint::textToUppercase(const QString& text) {
+    if (text != text.toUpper()) {
+        hintLineEdit->setText(text.toUpper());
+    }
 }
