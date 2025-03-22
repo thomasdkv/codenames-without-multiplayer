c9141fdf9a800de371287f6c9d725caa49aaee81
Matthew <matthewmarbina87@gmail.com>
2025-03-22 05:53:28 -0400

    MBA-13 created header file for spymasterhint class

---
+++ include/spymasterhint.h
@@ -0,0 +1,30 @@
+#ifndef SPYMASTERHINT_H
+#define SPYMASTERHINT_H
+
+#include <QWidget>
+#include <QLineEdit>
+#include <QSpinBox>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+class SpymasterHint : public QWidget {
+  Q_OBJECT
+
+ public:
+  explicit SpymasterHint(QWidget* parent = nullptr);
+  ~SpymasterHint();
+
+ signals:
+  void hintSubmitted(const QString& hint, const int number);
+
+ private slots:
+  void submitHint();
+
+ private:
+  QLineEdit* hintLineEdit;
+  QSpinBox* numberSpinBox;
+  QPushButton* giveClueButton;
+};
+
+
+#endif // SPYMASTERHINT_H
