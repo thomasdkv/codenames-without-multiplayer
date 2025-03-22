7a3cf56ee63adb29a3d0f508a876efe8b456756e
Matthew <matthewmarbina87@gmail.com>
2025-03-22 06:21:07 -0400

    MBA-13 created spymasterhint implementation file and also created constructor for the class

---
+++ src/spymasterhint.cpp
@@ -0,0 +1,30 @@
+#include "spymasterhint.h"
+
+SpymasterHint::SpymasterHint(QWidget* parent) : QWidget(parent) {
+    // Set up the layout
+    QHBoxLayout* layout = new QHBoxLayout(this);
+
+    // Create the LineEdit, SpinBox, and Button widgets
+    QLineEdit* enterClue = new QLineEdit(this);
+    enterClue->setPlaceholderText("Type your clue here");
+
+    QSpinBox* enterNumber = new QSpinBox(this);
+    enterNumber->setRange(0, 9);
+    enterNumber->setSpecialValueText("∞"); // Special value for unlimited guesses
+
+    QPushButton* giveClueButton = new QPushButton("Give Clue", this);
+
+    // Add the widgets to the layout
+    layout->addWidget(enterClue);
+    layout->addWidget(enterNumber);
+    layout->addWidget(giveClueButton);
+    setLayout(layout);
+
+    // Connect the button to the submitHint slot
+    connect(giveClueButton, &QPushButton::clicked, this, &SpymasterHint::submitHint);
+
+    // Store the widgets in the class
+    hintLineEdit = enterClue;
+    numberSpinBox = enterNumber;
+    giveClueButton = giveClueButton;
+}
