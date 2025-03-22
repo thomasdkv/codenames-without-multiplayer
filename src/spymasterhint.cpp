4e86892cb65692e62ffe445aef7823aad8ad30a2
Matthew <matthewmarbina87@gmail.com>
2025-03-22 10:24:21 -0400

    MBA-13 added more comments and edited styling, also connected textChanged signal to textToUppercase function

--- src/spymasterhint.cpp
+++ src/spymasterhint.cpp
@@ -7,26 +7,29 @@ SpymasterHint::SpymasterHint(QWidget* parent) : QWidget(parent) {
     // Create the LineEdit, SpinBox, and Button widgets
     hintLineEdit = new QLineEdit(this);
     hintLineEdit->setPlaceholderText("Type your clue here");
-    hintLineEdit->setFixedSize(300, 35);
+    hintLineEdit->setFixedSize(380, 40);
     hintLineEdit->setStyleSheet("text-transform: uppercase; padding: 5px; font-size: 16px;");
 
+    // Set up a validator to only allow text input and connect the textChanged signal to textToUppercase
     textValidator = new QRegularExpressionValidator(QRegularExpression("[A-Za-z]+"), this);
     hintLineEdit->setValidator(textValidator);
+    connect(hintLineEdit, &QLineEdit::textChanged, this, &SpymasterHint::textToUppercase);
 
+    // Create a SpinBox for the number of words
     numberSpinBox = new QSpinBox(this);
     numberSpinBox->setRange(0, 9);
     numberSpinBox->setSpecialValueText("∞"); // Special value for unlimited guesses
-    numberSpinBox->setFixedSize(50, 35);
+    numberSpinBox->setFixedSize(75, 40);
 
+    // Create a button to submit the hint
     giveClueButton = new QPushButton("Give Clue", this);
-    giveClueButton->setFixedSize(100, 35);
+    giveClueButton->setFixedSize(135, 40);
     giveClueButton->setStyleSheet(
-        "font-weight: bold; font-size: 16px; color: white; border-radius: 5px;"
-        "background-color:rgb(54, 182, 58);"
-        "QPushButton:hover { background-color: rgb(119, 236, 117); }");
+        "QPushButton { font-weight: bold; font-size: 16px; color: white; border-radius: 15px; "
+        "background-color: rgb(54, 182, 58); }"
+        "QPushButton:hover { background-color: rgb(54, 212, 58); }");
     giveClueButton->setEnabled(false);
     giveClueButton->setCursor(Qt::PointingHandCursor);
-
 
     // Add the widgets to the layout
     layout->addWidget(hintLineEdit);
@@ -34,7 +37,7 @@ SpymasterHint::SpymasterHint(QWidget* parent) : QWidget(parent) {
     layout->addWidget(giveClueButton);
     setLayout(layout);
 
-    // Connect the button to the submitHint slot
+    // Connect the button to the submitHint slot and the LineEdit to the updateButtonClickable slot
     connect(giveClueButton, &QPushButton::clicked, this, &SpymasterHint::submitHint);
     connect(hintLineEdit, &QLineEdit::textChanged, this, &SpymasterHint::updateButtonClickable);
 }
@@ -43,6 +46,7 @@ SpymasterHint::~SpymasterHint() {
     // Deconstructor for the SpymasterHint class, does nothing
 }
 
+// Submit the hint to the game board
 void SpymasterHint::submitHint() {
     QString hint = hintLineEdit->text();
     int number = numberSpinBox->value();
@@ -51,10 +55,12 @@ void SpymasterHint::submitHint() {
     numberSpinBox->setSpecialValueText("∞");
 }
 
+// Update the button clickable state based on the hint text
 void SpymasterHint::updateButtonClickable() {
     giveClueButton->setEnabled(!hintLineEdit->text().isEmpty());
 }
 
+// Convert the hint text to uppercase
 void SpymasterHint::textToUppercase(const QString& text) {
     if (text != text.toUpper()) {
         hintLineEdit->setText(text.toUpper());
