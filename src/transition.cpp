668189d36a2480e684d207a51757d812c644464e
Matthew <matthewmarbina87@gmail.com>
2025-03-23 16:16:11 -0400

    MBA-41 implemented constructor for transition.cpp

---
+++ src/transition.cpp
@@ -0,0 +1,25 @@
+#include "transition.h"
+
+Transition::Transition (QWidget* parent) : QWidget(parent) {
+    QVBoxLayout* layout = new QVBoxLayout(this);
+    messageLabel = new QLabel("P", this);
+    messageLabel->setAlignment(Qt::AlignCenter);
+    messageLabel->setStyleSheet("font-size: 24px; font-weight: bold;");
+
+    continueButton = new QPushButton("Continue", this);
+    continueButton->setFixedSize(180, 40);
+    continueButton->setStyleSheet(
+        "QPushButton { font-weight: bold; font-size: 16px; color: white; border-radius: 15px; "
+        "background-color: rgb(226, 226, 226); }"
+        "QPushButton:hover { background-color: rgb(238, 238, 238); }");
+    continueButton->setEnabled(true);
+    continueButton->setCursor(Qt::PointingHandCursor);
+
+    layout->addStretch();
+    layout->addWidget(messageLabel);
+    layout->addWidget(continueButton);
+    layout->addStretch();
+    setLayout(layout);
+
+    connect(continueButton, &QPushButton::clicked, this, &Transition::continueClicked);  
+}
