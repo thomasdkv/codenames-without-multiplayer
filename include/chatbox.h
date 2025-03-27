7b9d2e0745fa00f286af1787bd44653569a3ef7c
Matthew <matthewmarbina87@gmail.com>
2025-03-26 23:47:58 -0400

    MBA-31 implemented header file for chatbox class

---
+++ include/chatbox.h
@@ -0,0 +1,30 @@
+#ifndef CHATBOX_H
+#define CHATBOX_H
+
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QDateTime>
+
+class ChatBox : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit ChatBox(const QString& playerName, QWidget* parent = nullptr);
+    ~ChatBox();
+    void addSystemMessage(const QString& message);
+    void addPlayerMessage(const QString& playerName, const QString& message);
+
+public slots:
+    void sendMessage();
+
+private:
+    QTextEdit* chatDisplay;
+    QLineEdit* chatInput;
+    QPushButton* sendButton;
+    QString playerName;
+};
+
+#endif // CHATBOX_H
