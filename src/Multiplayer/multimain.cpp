c354e81b594b33313244dc0d29a19d46950eca76
Alex Hue Quang Nham <anham3@uwo.ca>
2025-03-30 04:15:37 -0400

    dynamic ports

--- src/Multiplayer/multimain.cpp
+++ src/Multiplayer/multimain.cpp
@@ -15,8 +15,7 @@ MultiMain::MultiMain(QWidget *parent)
     // Set background style
     this->setStyleSheet(
         "background-image: url(:/images/menu-background.png);"
-        "background-position: center;"
-    );
+        "background-position: center;");
 
     // Layout for widgets
     QVBoxLayout *layout = new QVBoxLayout(this);
@@ -27,33 +26,33 @@ MultiMain::MultiMain(QWidget *parent)
     titleLabel->setAlignment(Qt::AlignCenter);
     titleLabel->setStyleSheet("font-weight: bold; font-size: 50px;");
 
-   
     // Button styling
     QString buttonStyles = "QPushButton {"
-        "   background-color: rgb(65, 42, 213);"
-        "   color: white;"
-        "   border-radius: 5px;"
-        "   border: 2px solid #412AD5;"
-        "   padding: 5px;"
-        "   font-weight: bold;"
-        "   font-size: 20px;"
-        "}"
-        "QPushButton:hover {"
-        "   background-color: rgb(54, 35, 177);"
-        "}";
+                           "   background-color: rgb(65, 42, 213);"
+                           "   color: white;"
+                           "   border-radius: 5px;"
+                           "   border: 2px solid #412AD5;"
+                           "   padding: 5px;"
+                           "   font-weight: bold;"
+                           "   font-size: 20px;"
+                           "}"
+                           "QPushButton:hover {"
+                           "   background-color: rgb(54, 35, 177);"
+                           "}";
 
     // Function to create styled buttons with drop shadows
-    auto createButton = [this, &buttonStyles](const QString &text) {
+    auto createButton = [this, &buttonStyles](const QString &text)
+    {
         QPushButton *button = new QPushButton(text, this);
         button->setFixedSize(200, 50);
         button->setStyleSheet(buttonStyles);
-        
+
         QGraphicsDropShadowEffect *shadow = new QGraphicsDropShadowEffect(this);
         shadow->setBlurRadius(5);
         shadow->setOffset(0, 3);
         shadow->setColor(Qt::black);
         button->setGraphicsEffect(shadow);
-        
+
         return button;
     };
 
@@ -71,7 +70,8 @@ MultiMain::MultiMain(QWidget *parent)
     connect(createRoomButton, &QPushButton::clicked, this, &MultiMain::onCreateRoomClicked);
     connect(joinRoomButton, &QPushButton::clicked, this, &MultiMain::onJoinRoomClicked);
 
-    connect(this, &MultiMain::enterPregameAsHost, [this](QWebSocketServer* s, const QString& u) {
+    connect(this, &MultiMain::enterPregameAsHost, [this](QWebSocketServer *s, const QString &u)
+            {
     MultiPregame* pregame = new MultiPregame(s, u);
     pregame->show();
     this->hide();
@@ -81,12 +81,10 @@ MultiMain::MultiMain(QWidget *parent)
         pregame->close();
         pregame->deleteLater();
         this->show();
-    });
-});
+    }); });
 
-
-
-    connect(this, &MultiMain::enterPregameAsClient, [this](QWebSocket* s, const QString& u) {
+    connect(this, &MultiMain::enterPregameAsClient, [this](QWebSocket *s, const QString &u)
+            {
         MultiPregame* pregame = new MultiPregame(s, u);
         pregame->show();
         this->hide();
@@ -95,40 +93,45 @@ MultiMain::MultiMain(QWidget *parent)
             pregame->close();
             pregame->deleteLater();
             this->show();
-        });
-    });
-
+        }); });
 }
 
 MultiMain::~MultiMain()
 {
-    if(m_server) m_server->deleteLater();
-    if(m_clientSocket) m_clientSocket->deleteLater();
+    if (m_server)
+        m_server->deleteLater();
+    if (m_clientSocket)
+        m_clientSocket->deleteLater();
 }
 
-void MultiMain::openMainWindow() {
+void MultiMain::openMainWindow()
+{
     this->hide();
     emit backToMainWindow();
 }
 
-void MultiMain::showMainWindow() {
+void MultiMain::showMainWindow()
+{
     this->show();
 }
 
-void MultiMain::onCreateRoomClicked() {
+void MultiMain::onCreateRoomClicked()
+{
     // Load usernames from the JSON file
-    User* user = User::instance();
+    User *user = User::instance();
     QJsonObject jsonData = user->loadJsonFile();
-    
+
     QStringList usernames;
     QJsonObject::const_iterator it = jsonData.constBegin();
-    while (it != jsonData.constEnd()) {
-        usernames.append(it.key());  // Extract username keys
+    while (it != jsonData.constEnd())
+    {
+        usernames.append(it.key()); // Extract username keys
         ++it;
     }
 
     // If there are no usernames, show a message and return
-    if (usernames.isEmpty()) {
+    if (usernames.isEmpty())
+    {
         QMessageBox::warning(this, "No Users", "No user profiles found. Please create an account first.");
         return;
     }
@@ -159,25 +162,30 @@ void MultiMain::onCreateRoomClicked() {
     connect(&cancelButton, &QPushButton::clicked, &dialog, &QDialog::reject);
 
     // Execute dialog
-    if (dialog.exec() == QDialog::Accepted) {
+    if (dialog.exec() == QDialog::Accepted)
+    {
         QString username = comboBox.currentText();
-        if (username.isEmpty()) return;
+        if (username.isEmpty())
+            return;
 
         m_server = new QWebSocketServer("Game Server", QWebSocketServer::NonSecureMode, this);
 
-        if (m_server->listen(QHostAddress::AnyIPv4, 12345)) {
+        if (m_server->listen(QHostAddress::AnyIPv4, 0))
+        {                                                  // 0 means any open port
+            quint16 assignedPort = m_server->serverPort(); // Get the assigned port
             emit enterPregameAsHost(m_server, username);
             this->hide();
         }
     }
 }
 
+void MultiMain::onNewConnection()
+{
+    QWebSocket *client = m_server->nextPendingConnection();
 
-void MultiMain::onNewConnection() {
-    QWebSocket* client = m_server->nextPendingConnection();
-    
-    connect(client, &QWebSocket::textMessageReceived, 
-        this, [this, client](const QString &message) {
+    connect(client, &QWebSocket::textMessageReceived,
+            this, [this, client](const QString &message)
+            {
             if(message.startsWith("USERNAME:")) {
                 QString username = message.mid(9);
                 m_usernames[client] = username;
@@ -185,48 +193,52 @@ void MultiMain::onNewConnection() {
             }
             else {
                 processTextMessage(message);
-            }
-        });
-    
+            } });
+
     connect(client, &QWebSocket::disconnected, this, &MultiMain::socketDisconnected);
     m_clients.append(client);
 }
 
-void MultiMain::socketDisconnected() {
-    if(QWebSocket* client = qobject_cast<QWebSocket*>(sender())) {
+void MultiMain::socketDisconnected()
+{
+    if (QWebSocket *client = qobject_cast<QWebSocket *>(sender()))
+    {
         m_clients.removeAll(client);
         m_usernames.remove(client);
-    
+
         client->deleteLater();
     }
 }
 
-
-
-void MultiMain::onJoinRoomClicked() {
+void MultiMain::onJoinRoomClicked()
+{
     bool ok;
-    QString host = QInputDialog::getText(this, "Join Room", 
-                                       "Server IP:", QLineEdit::Normal, 
-                                       "localhost", &ok);
-    if (!ok || host.isEmpty()) return;
+    QString host = QInputDialog::getText(this, "Join Room",
+                                         "Server IP:", QLineEdit::Normal,
+                                         "localhost", &ok);
+    if (!ok || host.isEmpty())
+        return;
 
-    int port = QInputDialog::getInt(this, "Join Room", 
-                                   "Port:", 12345, 1, 65535, 1, &ok);
-    if (!ok) return;
+    int port = QInputDialog::getInt(this, "Join Room",
+                                    "Port:", 1, 1, 65535, 1, &ok);
+    if (!ok)
+        return;
 
     // Load usernames from the JSON file
-    User* user = User::instance();
+    User *user = User::instance();
     QJsonObject jsonData = user->loadJsonFile();
-    
+
     QStringList usernames;
     QJsonObject::const_iterator it = jsonData.constBegin();
-    while (it != jsonData.constEnd()) {
-        usernames.append(it.key());  // Extract username keys
+    while (it != jsonData.constEnd())
+    {
+        usernames.append(it.key()); // Extract username keys
         ++it;
     }
 
     // If there are no usernames, show a message and return
-    if (usernames.isEmpty()) {
+    if (usernames.isEmpty())
+    {
         QMessageBox::warning(this, "No Users", "No user profiles found. Please create an account first.");
         return;
     }
@@ -257,47 +269,55 @@ void MultiMain::onJoinRoomClicked() {
     connect(&cancelButton, &QPushButton::clicked, &dialog, &QDialog::reject);
 
     // Execute dialog
-    if (dialog.exec() != QDialog::Accepted) return;
+    if (dialog.exec() != QDialog::Accepted)
+        return;
 
     QString username = comboBox.currentText();
-    if (username.isEmpty()) return;
+    if (username.isEmpty())
+        return;
 
     m_username = username;
     m_clientSocket = new QWebSocket;
 
-    
-    connect(m_clientSocket, &QWebSocket::connected, this, [this]() {
+    connect(m_clientSocket, &QWebSocket::connected, this, [this]()
+            {
         m_clientSocket->sendTextMessage("USERNAME:" + m_username);
-        joinRoomButton->setEnabled(false);
-    });
+        joinRoomButton->setEnabled(false); });
 
-    connect(m_clientSocket, &QWebSocket::connected, this, [this, username]() {
+    connect(m_clientSocket, &QWebSocket::connected, this, [this, username]()
+            {
         m_clientSocket->sendTextMessage("USERNAME:" + username);
         emit enterPregameAsClient(m_clientSocket, username);
-        this->hide();
-    });
+        this->hide(); });
 
     connect(m_clientSocket, &QWebSocket::disconnected, this, &MultiMain::onDisconnected);
 
     m_clientSocket->open(QUrl(QString("ws://%1:%2").arg(host).arg(port)));
 }
 
-void MultiMain::onConnected() {
+void MultiMain::onConnected()
+{
     // Handled in lambda
 }
 
-void MultiMain::onDisconnected() {
-    if(m_clientSocket) {
+void MultiMain::onDisconnected()
+{
+    if (m_clientSocket)
+    {
         m_clientSocket->close();
     }
     QMessageBox::warning(this, "Disconnected", "Lost connection to room");
     joinRoomButton->setEnabled(true);
 }
 
-void MultiMain::processTextMessage(QString message) {
-    if(QWebSocket* sender = qobject_cast<QWebSocket*>(this->sender())) {
-        for(QWebSocket* client : m_clients) {
-            if(client != sender) {
+void MultiMain::processTextMessage(QString message)
+{
+    if (QWebSocket *sender = qobject_cast<QWebSocket *>(this->sender()))
+    {
+        for (QWebSocket *client : m_clients)
+        {
+            if (client != sender)
+            {
                 client->sendTextMessage(message);
             }
         }
